4799339dbf835ef7de6543ffb9aceb0be87b49ad
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-04-24 16:49:33 +0430

    write addOrdinalSuffix's doc

--- src/main/kotlin/dev/persianTools/feature/ordinalSuffix/OrdinalSuffix.kt
+++ src/main/kotlin/dev/persianTools/feature/ordinalSuffix/OrdinalSuffix.kt
@@ -2,6 +2,13 @@ package dev.persianTools.feature.ordinalSuffix
 
 
 object OrdinalSuffix {
+    /**
+     * add ordinal suffix to the given string number
+     * @param string [String] number
+     * @return Suffix [String]
+     *
+     * @author Moein
+     */
     fun addOrdinalSuffix(string: String): String {
         return when {
             string.isBlank() -> {
@@ -16,6 +23,13 @@ object OrdinalSuffix {
             else -> string + "م"
         }
     }
+
+
 }
 
+/**
+ * Add ordinal suffix extension
+ * This extension uses [OrdinalSuffix.addOrdinalSuffix] function
+ *
+ */
 fun String.addOrdinalSuffix(): String = OrdinalSuffix.addOrdinalSuffix(this)
