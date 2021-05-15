3cc9cb3c071bf10bf787e05e95fa22debf8e6fc8
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-05-15 15:10:35 +0430

    feat(isPersian): write isPersian extension functions

--- src/main/kotlin/dev/persianTools/feature/isPersian/FarsiCharacters.kt
+++ src/main/kotlin/dev/persianTools/feature/isPersian/FarsiCharacters.kt
@@ -4,13 +4,13 @@ object FarsiCharacters {
 
     private val defaultTrimPattern: Regex = Regex("[^A-Za-z0-9 ]")
 
-    fun isPersian(input: String, isComplex: Boolean, trimPattern: Regex = defaultTrimPattern): Boolean {
+    fun isPersian(input: String, isComplex: Boolean = false, trimPattern: Regex = defaultTrimPattern): Boolean {
         val rawText = input.replace(trimPattern,"")
         val faRegexExp = getFaRegexExp(isComplex)
         return rawText.matches(faRegexExp)
     }
 
-    fun hasPersian(input: String, isComplex: Boolean): Boolean {
+    fun hasPersian(input: String, isComplex: Boolean = false): Boolean {
         val faRegexExp = getFaRegexExp(isComplex)
         return input.matches(faRegexExp)
     }
@@ -20,3 +20,17 @@ object FarsiCharacters {
         return Regex(faRegexExp)
     }
 }
+
+/**
+ * Check is the input string Persian or not
+ * returns true if the input is matched with Farsi char dataset [Regex],
+ * if not returns false
+ */
+fun String.isPersian(isComplex: Boolean = false) = FarsiCharacters.isPersian(this, isComplex)
+
+/**
+ * Check is the input string includes Persian alphabet or not
+ * returns true if the input is matched with Farsi char dataset [Regex],
+ * if not returns false
+ */
+fun String.hasPersian(isComplex: Boolean = false) = FarsiCharacters.hasPersian(this, isComplex)
